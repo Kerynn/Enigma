4985b5ba619f97856d46f4de1970d0566a4dfb27
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-10 15:48:32 -0800

    feat: Enigma class initialized

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,7 +1,12 @@
 require './spec/spec_helper'
+require './lib/enigma'
 
-Rspec.describe Enigma do
-  it 'exists' do 
+RSpec.describe Enigma do
+  it 'exists' do
+    enigma = Enigma.new
+
+    expect(enigma).to be_a(Enigma)
+  end
 
 
 
