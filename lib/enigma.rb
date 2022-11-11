17007fbe5e42b0a0d4f4216bae857d9ca3e01f9d
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-10 16:11:40 -0800

    test: Set up encrypt method, need other methods first

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,4 +4,11 @@ class Enigma
     ('a'..'z').to_a << ' '
   end
 
+  def encrypt(message, key, date)
+    encrypt_hash = {}
+    encrypt_hash[:encryption] = message #encrypted message, method
+    encrypt_hash[:key] = key
+    encrypt_hash[:date] = date
+    encrypt_hash
+  end
 end
