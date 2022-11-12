6b11eff1fd726e6118107de49dd49db960500343
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-12 10:36:56 -0800

    refactor: Splitting up shift_message method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -95,33 +95,33 @@ class Enigma
     end
   end
 
-  def shift_message(message, keys)
-    msg_array = split_msg(message)
-    a_array = []
-    b_array = []
-    c_array = []
-    d_array = []
-    until msg_array.empty? do
-      a_array << msg_array[0]
-      b_array << msg_array[1] if msg_array[1]
-      c_array << msg_array[2] if msg_array[2]
-      d_array << msg_array[3] if msg_array[3]
-      msg_array.shift(4)
-    end
-    keys_array = [[a_array, keys[:A]], [b_array, keys[:B]], [c_array, keys[:C]], [d_array, keys[:D]]]
-    final = keys_array.map do |msg_char, value|
-      msg_char.map do |char|
-      indexed_position = (characters.find_index(char) + value)
-      characters[indexed_position]
-    end
-  end
-  string_char = ''
-    until final.join.empty? do
-      final.map.with_index do |shift_char, index|
-        string_char << shift_char[0] if shift_char[0]
-        shift_char.shift
-       end
-    end
-    string_char
-  end
+  # def shift_message(message, keys)
+  #   msg_array = split_msg(message)
+  #   a_array = []
+  #   b_array = []
+  #   c_array = []
+  #   d_array = []
+  #   until msg_array.empty? do
+  #     a_array << msg_array[0]
+  #     b_array << msg_array[1] if msg_array[1]
+  #     c_array << msg_array[2] if msg_array[2]
+  #     d_array << msg_array[3] if msg_array[3]
+  #     msg_array.shift(4)
+  #   end
+  #   keys_array = [[a_array, keys[:A]], [b_array, keys[:B]], [c_array, keys[:C]], [d_array, keys[:D]]]
+  #   final = keys_array.map do |msg_char, value|
+  #     msg_char.map do |char|
+  #     indexed_position = (characters.find_index(char) + value)
+  #     characters[indexed_position]
+  #   end
+  # end
+  # string_char = ''
+  #   until final.join.empty? do
+  #     final.map.with_index do |shift_char, index|
+  #       string_char << shift_char[0] if shift_char[0]
+  #       shift_char.shift
+  #      end
+  #   end
+  #   string_char
+  # end
 end
